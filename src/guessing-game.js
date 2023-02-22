41d7eb4ae126488ecc295508f719bf932e2788e4
Kirill Semkin <93377014+Kirilish@users.noreply.github.com>
2023-02-22 21:10:21 +0300

    Update guessing-game.js

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+    this.min = null;
+    this.max = null;
+    this.guessNumber = null;
+    }
 
     setRange(min, max) {
-
+    this.min = min;
+    this.max = max;
     }
 
     guess() {
-
+    this.guessNumber = Math.floor((this.min + this.max) / 2);
+    return this.guessNumber;
     }
 
     lower() {
-
+    this.max = this.guessNumber;
     }
 
     greater() {
-
+    this.min = this.guessNumber;
     }
 }
 
